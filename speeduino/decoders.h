bd4ba72508d1b11cda9d81dec39776bced9674ad
ruckusman <glennrbarry@gmail.com>
2025-12-09 23:43:36 +1100

    Update decoders.h
    Added #define entry for the Yamaha FZR250 3LN1
    Added entry for the Yamaha FZR250 3LN1

--- speeduino/decoders.h
+++ speeduino/decoders.h
@@ -42,6 +42,7 @@
 #define DECODER_SUZUKI_K6A        26
 #define DECODER_HONDA_J32         27
 #define DECODER_FORD_TFI          28
+#define DECODER_FZR250_3LN1       29
 
 #define BIT_DECODER_2ND_DERIV           0 //The use of the 2nd derivative calculation is limited to certain decoders. This is set to either true or false in each decoders setup routine
 #define BIT_DECODER_IS_SEQUENTIAL       1 //Whether or not the decoder supports sequential operation
@@ -277,6 +278,10 @@ void triggerSec_FordTFI(void);
 uint16_t getRPM_FordTFI(void);
 int getCrankAngle_FordTFI(void);
 void triggerSetEndTeeth_FordTFI(void);
+
+void triggerSetup_FZR250_3LN1(void);
+void triggerPri_FZR250_3LN1(void);
+void triggerSetEndTeeth_FZR250_3LN1(void);
 
 /**
  * @brief This function is called when the engine is stopped, or when the engine is started. It resets the decoder state and the tooth tracking variables
