7cb6347574867f45ad05a08a0d3a32cebdf7e2f8
ruckusman <glennrbarry@gmail.com>
2025-12-10 09:03:59 +1100

    Update decoders.h
    Corrected 3LNI error to 3LN1

--- speeduino/decoders.h
+++ speeduino/decoders.h
@@ -279,11 +279,11 @@ uint16_t getRPM_FordTFI(void);
 int getCrankAngle_FordTFI(void);
 void triggerSetEndTeeth_FordTFI(void);
 
-void triggerSetup_FZR250_3LNI(void);
-void triggerPri_FZR250_3LNI(void);
-uint16_t getRPM_FZR250_3LNI(void);
-int getCrankAngle_FZR250_3LNI(void);
-void triggerSetEndTeeth_FZR250_3LNI(void);
+void triggerSetup_FZR250_3LN1(void);
+void triggerPri_FZR250_3LN1(void);
+uint16_t getRPM_FZR250_3LN1(void);
+int getCrankAngle_FZR250_3LN1(void);
+void triggerSetEndTeeth_FZR250_3LN1(void);
 
 /**
  * @brief This function is called when the engine is stopped, or when the engine is started. It resets the decoder state and the tooth tracking variables
