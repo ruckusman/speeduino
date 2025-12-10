ab872d84a277dc81e46bfd635bd15b222870a3e7
ruckusman <glennrbarry@gmail.com>
2025-12-10 16:15:59 +1100

    Update comms_legacy.cpp
    Speeduino 2025.04-dev

--- speeduino/comms_legacy.cpp
+++ speeduino/comms_legacy.cpp
@@ -653,7 +653,7 @@ void legacySerialHandler(byte cmd, Stream &targetPort, SerialStatus &targetStatu
       break;
 
     case 'Q': // send code version
-      targetPort.print(F("speeduino 202501"));
+      targetPort.print(F("Speeduino 2025.04-dev"));
       break;
 
     case 'r': //New format for the optimised OutputChannels
@@ -685,7 +685,7 @@ void legacySerialHandler(byte cmd, Stream &targetPort, SerialStatus &targetStatu
       break;
 
     case 'S': // send code version
-      targetPort.print(F("Speeduino 202501"));
+      targetPort.print(F("Speeduino 2025.04-dev"));
       break;
   }
 }
